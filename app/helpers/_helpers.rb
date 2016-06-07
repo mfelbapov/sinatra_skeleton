db9c1b9d30a7268d7ed28020c495f0dd29e3c2b5
Milorad Felbapov <mfelbapov@gmail.com>
2016-06-07 08:55:28 -0500

    Add files via upload

---
+++ app/helpers/_helpers.rb
@@ -0,0 +1,28 @@
+helpers do
+  def current_user
+    if !!session[:user_id]
+      @current_user ||= User.find_by_id(session[:user_id])
+    end
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def ensure_login
+    if logged_in?
+      true
+    else
+      halt(404, "<h1>No Way Jose</h1>" )
+    end
+  end
+
+  def author?(blog)
+    if !!current_user
+      blog.author_id == current_user.id
+    else
+      false
+    end
+  end
+
+end
---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ActiveRecord::Base
+
+  def password
+    @password ||= BCrypt::Password.new(hashed_password)
+  end
+
+  def password=(new_password)
+    @password = BCrypt::Password.create(new_password)
+    self.hashed_password = @password
+  end
+
+  def authenticate?(email, password)
+    self.password == password
+  end
+end
